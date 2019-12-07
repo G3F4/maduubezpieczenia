6d1955bf0acead5c95f1604ebc801d0342ebee2e
G3F4 <kamil.grabek@gmail.com>
2019-12-07 14:51:09 +0100

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-# Madu Ubezpieczenia
---
+++ precache-manifest.a069e942859c4a2647ede80fc8bed725.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "640c6ecac7b71aa8e165",
+    "url": "/maduubezpieczenia/static/css/main.64da09c9.chunk.css"
+  },
+  {
+    "revision": "640c6ecac7b71aa8e165",
+    "url": "/maduubezpieczenia/static/js/main.640c6eca.chunk.js"
+  },
+  {
+    "revision": "74c01edd3da77396f15c",
+    "url": "/maduubezpieczenia/static/js/runtime~main.74c01edd.js"
+  },
+  {
+    "revision": "5ca1dc633b18e891d291",
+    "url": "/maduubezpieczenia/static/js/2.5ca1dc63.chunk.js"
+  },
+  {
+    "revision": "e23b7c6d152be607141cee175ddd74fd",
+    "url": "/maduubezpieczenia/index.html"
+  }
+];
