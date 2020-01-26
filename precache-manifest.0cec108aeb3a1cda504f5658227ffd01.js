99195aba1106146f989bd5108bad77b6cad642d7
G3F4 <kamil.grabek@gmail.com>
2020-01-26 13:46:14 +0100

    Updates

--- precache-manifest.0cec108aeb3a1cda504f5658227ffd01.js
+++ precache-manifest.0cec108aeb3a1cda504f5658227ffd01.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "629e9e0186c6c8057183",
+    "revision": "d727a8e3269c32c957b8",
     "url": "/maduubezpieczenia/static/css/main.fb39885c.chunk.css"
   },
   {
-    "revision": "629e9e0186c6c8057183",
-    "url": "/maduubezpieczenia/static/js/main.629e9e01.chunk.js"
+    "revision": "d727a8e3269c32c957b8",
+    "url": "/maduubezpieczenia/static/js/main.d727a8e3.chunk.js"
   },
   {
     "revision": "74c01edd3da77396f15c",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/maduubezpieczenia/static/js/2.3d89b5cd.chunk.js"
   },
   {
-    "revision": "a3e87777a525c4df30f804d7820c1305",
+    "revision": "de0e8db9a81145b7aec67f3f4d9fd985",
     "url": "/maduubezpieczenia/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/maduubezpieczenia/precache-manifest.c13a892265475a4882edacb821266d69.js"
+  "/maduubezpieczenia/precache-manifest.0cec108aeb3a1cda504f5658227ffd01.js"
 );
 
 workbox.clientsClaim();
