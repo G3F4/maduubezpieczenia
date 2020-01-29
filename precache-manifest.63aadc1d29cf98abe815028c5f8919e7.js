2fcfb41d764554d6e1df907a0671b4643aec9d07
G3F4 <kamil.grabek@gmail.com>
2020-01-29 18:23:06 +0100

    Updates

---
+++ precache-manifest.63aadc1d29cf98abe815028c5f8919e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a85b52d63baa10a6f151",
+    "url": "ubezpieczeniamadu.pl/static/css/main.ccdb6dda.chunk.css"
+  },
+  {
+    "revision": "a85b52d63baa10a6f151",
+    "url": "ubezpieczeniamadu.pl/static/js/main.a85b52d6.chunk.js"
+  },
+  {
+    "revision": "4fda29418ced699dc2ce",
+    "url": "ubezpieczeniamadu.pl/static/js/runtime~main.4fda2941.js"
+  },
+  {
+    "revision": "3d89b5cdda085d413432",
+    "url": "ubezpieczeniamadu.pl/static/js/2.3d89b5cd.chunk.js"
+  },
+  {
+    "revision": "371af57a906c88860534f617512afa75",
+    "url": "ubezpieczeniamadu.pl/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.accc2c296d24e256c5065feb5703ecc3.js"
+  "ubezpieczeniamadu.pl/precache-manifest.63aadc1d29cf98abe815028c5f8919e7.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("ubezpieczeniamadu.pl/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
