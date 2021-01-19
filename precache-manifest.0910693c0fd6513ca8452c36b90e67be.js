d3fa58f9eeb5e912460e16884c74f25e0a4c89a7
Kamil Grabek <kamil.grabek@allegro.pl>
2021-01-19 18:05:46 +0100

    Updates

--- precache-manifest.0910693c0fd6513ca8452c36b90e67be.js
+++ precache-manifest.0910693c0fd6513ca8452c36b90e67be.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "cd21c7f488c4db5f7810",
+    "revision": "2092a23b42c8982fcbc8",
     "url": "/static/css/main.7301732c.chunk.css"
   },
   {
-    "revision": "cd21c7f488c4db5f7810",
-    "url": "/static/js/main.cd21c7f4.chunk.js"
+    "revision": "2092a23b42c8982fcbc8",
+    "url": "/static/js/main.2092a23b.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/static/js/2.db50dfcc.chunk.js"
   },
   {
-    "revision": "5239e5b97f7d84801d38e975ab48723b",
+    "revision": "b1ba37ed1ff46282b0b42f40a15fb5e0",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9034f0fd5911d6645fe6517da47d6bb4.js"
+  "/precache-manifest.0910693c0fd6513ca8452c36b90e67be.js"
 );
 
 workbox.clientsClaim();
